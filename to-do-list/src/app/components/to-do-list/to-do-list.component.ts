9108b98f4939376ee79bc02b4d13862a9748136a
dmitry.koryanov <dmitry.koryanov@yandex.ru>
2024-12-05 16:45:18 +0100

    ДЗ#3: Добавляем анимацию загрузки (имитируем подгрузку данных с бекэнда). Используем shared модуль.

--- to-do-list/src/app/components/to-do-list/to-do-list.component.ts
+++ to-do-list/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 export interface ListItem {
   id: number;
@@ -11,15 +16,23 @@ export interface ListItem {
   styleUrl: './to-do-list.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class ToDoListComponent {
+export class ToDoListComponent implements OnInit {
   @HostBinding('class') cssClass = 'app-to-do-list';
   inputText = '';
+
+  isLoading: boolean = true;
 
   listItems: ListItem[] = [
     { id: 1, text: 'Buy a new gaming laptop' },
     { id: 2, text: 'Complete previous task' },
     { id: 3, text: 'Create some angualr app' },
   ];
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 500);
+  }
 
   isAddTaskButtonDisabled(): boolean {
     return this.inputText.length === 0;
@@ -31,6 +44,10 @@ export class ToDoListComponent {
   }
 
   addItem() {
+    if (this.inputText.length === 0) {
+      return;
+    }
+
     const maxId = Math.max(...this.listItems.map((listItem) => listItem.id));
     this.listItems.push({ id: maxId + 1, text: this.inputText });
     this.inputText = '';
